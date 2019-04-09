8ff6e9185848e8c1dd305938fc108c2473eb5a0d
Miina Pulkkinen <44865291+pulmii@users.noreply.github.com>
2019-04-09 19:26:22 +0300

    Update table_TYONTEKIJA.sql

--- table_TYONTEKIJA.sql
+++ table_TYONTEKIJA.sql
@@ -3,7 +3,7 @@ tyontekija_id integer primary key,
 sukunimi varchar(20) not null,
 etunimi varchar(20) not null,
 hetu varchar(11) not null,
-palkka double(7),
+palkka float,
 syntymaaika date,
 osasto integer,
 )
