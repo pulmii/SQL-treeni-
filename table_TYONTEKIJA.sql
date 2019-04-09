b071bb842ec1c7a8015c61cc0158ac8e6b688a3a
Miina Pulkkinen <44865291+pulmii@users.noreply.github.com>
2019-04-09 19:24:15 +0300

    Update table_TYONTEKIJA.sql

--- table_TYONTEKIJA.sql
+++ table_TYONTEKIJA.sql
@@ -3,7 +3,35 @@ tyontekija_id integer primary key,
 sukunimi varchar(20) not null,
 etunimi varchar(20) not null,
 hetu varchar(11) not null,
-palkka DOUBLE PRESICION,
+palkka double,
 syntymaaika date,
 osasto integer,
 )
+
+insert into TYONTEKIJA
+(tyontekija_id, etunimi, sukunimi, hetu, palkka, syntymaaika, osasto)
+values
+(544, 'Venla', 'Virta', '120272-246W', 2310.10, 1972-02-12, 8)
+
+insert into TYONTEKIJA
+values
+(544, 'Maija', 'Mallikas', '190280-216L', 2310.20, 1980-02-19, 7),
+(540, 'Touko', 'Nieminen', '121285-128Y', 2410.00, 1985-12-12, 1),
+(539, 'Vieno', 'Virtanen', '100558-246W', 2710.80, 1958-05-10, 4),
+(548, 'Antti', 'Antikainen', '120165-333H', 2210.00, 1965-01-22, 8)
+
+update TYONTEKIJA
+set sukunimi = 'Korhonen'
+where tyontekija_id = 544
+
+	
+SELECT etunimi, sukunimi, osasto
+FROM TYONTEKIJA
+WHERE osasto = 8
+ORDER BY sukunimi asc
+
+SELECT MAX(palkka)
+FROM TYONTEKIJA; 
+
+SELECT SUM(palkka)
+FROM TYONTEKIJA
